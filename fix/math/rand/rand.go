fb25e133af3d21904d46a5730d0c5c44a558a969
Chris Pennello <git@chrispennello.com>
2015-05-23 12:38:59 -0700

    More documentation cleanup.

--- fix/math/rand/rand.go
+++ fix/math/rand/rand.go
@@ -5,8 +5,11 @@ package rand
 import "math/rand"
 
 // Uint64 returns a pseudo-random 64-bit value as a uint64 from the
-// default Source.  Calls Uint32, shifts, and ors with another call to
-// Uint32.
+// default Source.
+//
+// Calls Uint32, shifts, and ors with another call to Uint32.
+//
+// See:
 // https://groups.google.com/d/topic/golang-nuts/Kle874lT1Eo/discussion
 func Uint64() uint64 {
 	return uint64(rand.Uint32())<<32 | uint64(rand.Uint32())
