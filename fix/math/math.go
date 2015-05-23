9a8d96e013696e600321c055582f202f8324ab67
Chris Pennello <git@chrispennello.com>
2015-05-23 10:19:15 -0700

    fix/math: Revises Round function.
    Changes to float output.
    Adds handlingi for float special cases.
    Adds documentation.

--- fix/math/math.go
+++ fix/math/math.go
@@ -4,6 +4,14 @@ package math
 
 import "math"
 
-func Round(x float64) int {
-	return int(x + math.Copysign(.5, x))
+// Round returns x, rounded to the nearest integer.
+//
+// Special cases are:
+//	Round(±Inf) = ±Inf
+//	Round(NaN) = NaN
+func Round(x float64) float64 {
+	if math.IsNaN(x) || math.IsInf(x, 0) {
+		return x
+	}
+	return math.Trunc(x + math.Copysign(.5, x))
 }
--- fix/math/math_test.go
+++ fix/math/math_test.go
@@ -2,17 +2,45 @@
 
 package math
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func testRound(t *testing.T, x float64, check int) {
+func testRound(t *testing.T, x, check float64) {
 	out := Round(x)
 	if out != check {
 		t.Errorf("Round(%v) != %v (is %v)", x, check, out)
 	}
 }
 
+func testRoundInf(t *testing.T, sign int) {
+	x := math.Inf(sign)
+	out := Round(x)
+	if !math.IsInf(Round(math.Inf(sign)), sign) {
+		t.Errorf("Round(%v) \"!=\" %v (is %v)", x, x, out)
+	}
+}
+
+func testRoundNaN(t *testing.T) {
+	x := math.NaN()
+	out := Round(x)
+	if !math.IsNaN(out) {
+		t.Errorf("Round(%v) \"!=\" %v (is %v)", x, x, out)
+	}
+}
+
 func TestRound(t *testing.T) {
+	testRoundInf(t, 1)
+	testRoundInf(t, -1)
+
+	testRoundNaN(t)
+
+	testRound(t, -0, -0)
+	testRound(t, 0, -0)
+	testRound(t, -0, 0)
 	testRound(t, 0, 0)
+
 	testRound(t, .25, 0)
 	testRound(t, .5, 1)
 	testRound(t, .75, 1)
