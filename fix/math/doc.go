32291b291fbfd518a37ccb2eaad0b6dd3ae3ab3b
Chris Pennello <git@chrispennello.com>
2015-05-29 19:54:11 -0700

    Adds more package documentation.
    fix/math/doc.go
    fix/math/rand/doc.go

---
+++ fix/math/doc.go
@@ -0,0 +1,5 @@
+// chris 052915
+
+// Package math implements fixes or missing features from the Go
+// standard library.
+package math
---
+++ fix/math/rand/doc.go
@@ -0,0 +1,5 @@
+// chris 052915
+
+// Package rand implements fixes or missing features from the Go
+// standard library.
+package rand
