9ff809b14d837b240710c5197e1c820efdac5d99
Chris Pennello <git@chrispennello.com>
2015-07-28 10:18:32 -0700

    fix/math/rand: Adds Choose* functions.

--- fix/math/rand/rand.go
+++ fix/math/rand/rand.go
@@ -1,8 +1,18 @@
-// chris 0502115
+// chris 052115 Uint64.
+// chris 072315 Choose functions.
 
 package rand
 
-import "math/rand"
+import (
+	"errors"
+	"fmt"
+
+	"math/rand"
+)
+
+// ErrBadRange is returned by ChooseString when the input strings do not
+// contain the same number of runes.
+var ErrBadRange = errors.New("bad range")
 
 // Uint64 returns a pseudo-random 64-bit value as a uint64 from the
 // default Source.
@@ -13,4 +23,48 @@ import "math/rand"
 // https://groups.google.com/d/topic/golang-nuts/Kle874lT1Eo/discussion
 func Uint64() uint64 {
 	return uint64(rand.Uint32())<<32 | uint64(rand.Uint32())
+}
+
+// ChooseInt32 returns, as an int32, a pseudo-random integer in
+// [begin, end] from the default Source.  It panics if begin > end.
+//
+// This is bit width-specific because it uses a 64-bit integer to
+// contain the difference between begin and end.
+func ChooseInt32(begin, end int32) int32 {
+	if begin > end {
+		panic(fmt.Sprintf("invalid arguments to ChooseInt32: begin %d > end %d", begin, end))
+	}
+	diff := int64(end) - int64(begin)
+	return int32(int64(begin) + rand.Int63n(diff+1))
+}
+
+// ChooseRune returns a pseudo-random rune that is lexically and
+// inclusively between begin and end.  Specifically, the output x
+// satisfies begin <= x <= end.
+func ChooseRune(begin, end rune) rune {
+	return rune(ChooseInt32(int32(begin), int32(end)))
+}
+
+// ChooseString returns a pseudo-random string that is lexically and
+// inclusively between begin and end, using the default Source.
+// Specifically, the output x satisfies begin <= x <= end.
+//
+// If the begin and end strings don't contain the same number of runes,
+// the empty string is returned with ErrBadRange.  If they do, but at a
+// particular index, end contains a rune less than the corresponding
+// rune in begin, the empty string is returned with ErrBadRange.
+func ChooseString(begin, end string) (string, error) {
+	br := []rune(begin)
+	er := []rune(end)
+	if len(br) != len(er) {
+		return "", ErrBadRange
+	}
+	ret := make([]rune, len(br))
+	for i := range ret {
+		if int32(br[i]) > int32(er[i]) {
+			return "", ErrBadRange
+		}
+		ret[i] = ChooseRune(br[i], er[i])
+	}
+	return string(ret), nil
 }
--- fix/math/rand/rand_test.go
+++ fix/math/rand/rand_test.go
@@ -2,9 +2,66 @@
 
 package rand
 
-import "testing"
+import (
+	"testing"
+
+	"math/rand"
+)
 
 func TestUint64(t *testing.T) {
 	// Just make sure we can call it.
 	t.Log(Uint64())
+}
+
+func testChooseInt32(t *testing.T, begin, end int32) {
+	x := ChooseInt32(begin, end)
+	if !(begin <= x && x <= end) {
+		t.Fail()
+	}
+}
+
+func testChooseInt32Panic(t *testing.T, begin, end int32) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fail()
+		}
+	}()
+	ChooseInt32(begin, end)
+}
+
+func TestChooseInt32(t *testing.T) {
+	testChooseInt32(t, 0, 0)
+	testChooseInt32(t, 0, 100)
+	testChooseInt32(t, 100, 100)
+	testChooseInt32(t, -100, 100)
+	testChooseInt32(t, -200, -100)
+
+	for i := 0; i < 1000; i++ {
+		begin := int32(rand.Uint32())
+		var end int32
+		for {
+			end = int32(rand.Uint32())
+			if begin <= end {
+				break
+			}
+		}
+		testChooseInt32(t, begin, end)
+	}
+
+	testChooseInt32Panic(t, 1, 0)
+	testChooseInt32Panic(t, 1, -1)
+	testChooseInt32Panic(t, -1, -2)
+	testChooseInt32Panic(t, 2, 1)
+}
+
+func testChooseStringErr(t *testing.T, begin, end string) {
+	_, err := ChooseString(begin, end)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestChooseString(t *testing.T) {
+	testChooseStringErr(t, "z", "a")
+	testChooseStringErr(t, "az", "ba")
 }
